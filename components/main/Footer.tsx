8dfaa3667b7afdf93f2a5cc250916d0d1e7f929f
Paul-99-art <karaneugenepetrova@gmail.com>
2025-04-11 15:17:25 -0700

    did some changes

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
                     <div className="font-bold text-[16px]">Social Media</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
+                        <span className="text-[15px] ml-[6px]">Github</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxDiscordLogo />
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
+import Link from 'next/link';
 
 const Projects = () => {
   return (
@@ -11,16 +12,18 @@ const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
+      <Link href="https://jsm-mock-interview-platform-nine.vercel.app">
+  <ProjectCard
+    src="/NextWebsite.png"
+    title="MockMate - AI Interview Prep"
+    description="MockMate is an interview preparation application with AI voice assistants that streamlines the interview practice process."
+  />
+</Link>
+        {/* <ProjectCard
           src="/CardImage.png"
           title="Interactive Website Cards"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+        /> */}
         <ProjectCard
           src="/SpaceWebsite.png"
           title="Space Themed Website"
